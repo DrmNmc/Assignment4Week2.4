859e707d0eb066de1c483d2e8bc3721afee4bb95
jibbajab <dylan.r.miller517@outlook.com>
2023-04-17 03:50:18 -0400

    init

--- Assignment4Week2/Program.cs
+++ Assignment4Week2/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
-namespace ThreadRace
+namespace taskRace
 {
     internal class Program
     {
@@ -14,8 +15,8 @@ namespace ThreadRace
             int t4Location = 0;
             int t5Location = 0;
 
-            //Creating Threads
-            Thread t1 = new Thread(delegate ()
+            // Creating Tasks
+            Task t1 = Task.Run(() =>
             {
                 Thread.CurrentThread.Name = "Speedy Gonzales";
                 for (int i = 0; i < 100; i++)
@@ -23,19 +24,19 @@ namespace ThreadRace
                     if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
                         MoveIt(ref t1Location);
                 }
-
             });
-            Thread t2 = new Thread(delegate ()
+
+            Task t2 = Task.Run(() =>
             {
                 Thread.CurrentThread.Name = "Road Runner";
                 for (int i = 0; i < 100; i++)
                 {
                     if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
                         MoveIt(ref t2Location);
-
                 }
             });
-            Thread t3 = new Thread(delegate ()
+
+            Task t3 = Task.Run(() =>
             {
                 Thread.CurrentThread.Name = "The Flash";
                 for (int i = 0; i < 100; i++)
@@ -44,7 +45,8 @@ namespace ThreadRace
                         MoveIt(ref t3Location);
                 }
             });
-            Thread t4 = new Thread(delegate ()
+
+            Task t4 = Task.Run(() =>
             {
                 Thread.CurrentThread.Name = "Sonic";
                 for (int i = 0; i < 100; i++)
@@ -53,7 +55,8 @@ namespace ThreadRace
                         MoveIt(ref t4Location);
                 }
             });
-            Thread t5 = new Thread(delegate ()
+
+            Task t5 = Task.Run(() =>
             {
                 Thread.CurrentThread.Name = "Quicksilver";
                 for (int i = 0; i < 100; i++)
@@ -63,22 +66,11 @@ namespace ThreadRace
                 }
             });
 
-            t1.Priority = ThreadPriority.AboveNormal;
-
-            t1.Start();
-            t2.Start();
-            t3.Start();
-            t4.Start();
-            t5.Start();
-
-            t1.Join();
-            t2.Join();
-            t3.Join();
-            t4.Join();
-            t5.Join();
+            Task.WaitAny(t1, t2, t3, t4, t5);
 
             Console.WriteLine("Race has ended");
         }
+
         static void MoveIt(ref int location)
         {
             location++;
